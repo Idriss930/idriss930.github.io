0d0b4eb4028fdb3eed311a3e8958e753923f6256
Idriss930 <146744616+Idriss930@users.noreply.github.com>
2023-12-21 17:00:48 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+function toggleDetails(projectId) {
+    var projectDetails = document.getElementById(projectId);
+    if (projectDetails.style.display === 'none' || projectDetails.style.display === '') {
+        projectDetails.style.display = 'block';
+    } else {
+        projectDetails.style.display = 'none';
+    }
+}
